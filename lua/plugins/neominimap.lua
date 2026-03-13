2f5f1bcaf86235c045b20bca0433ea210bf0df68
minhnh <minhnh.da@gmail.com>
2026-03-13 22:48:15 +0900

    make minimap lazy loading

--- lua/plugins/neominimap.lua
+++ lua/plugins/neominimap.lua
@@ -2,7 +2,7 @@
 return {
   "Isrothy/neominimap.nvim",
   version = "v3.x.x",
-  lazy = false, -- NOTE: NO NEED to Lazy load
+  event = "VeryLazy",
   -- Optional. You can also set your own keybindings
   keys = {
     -- Global Minimap Controls
